83eed3f95e0330753e40d9a9935c090d46d74ae9
DESKTOP-QUHF5E2\AMIT <amitsalunke01@gmail.com>
2020-11-18 02:18:13 +0530

    Added periodic work request example and getting data from worker to activity

--- app/src/main/java/com/amitsalunke/workmanager/MainActivity.kt
+++ app/src/main/java/com/amitsalunke/workmanager/MainActivity.kt
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.work.*
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
@@ -32,7 +34,7 @@ class MainActivity : AppCompatActivity() {
             .build()
 
         val data: Data = Data.Builder()
-            .putInt(KEY_COUNT_VALUE,1000)
+            .putInt(KEY_COUNT_VALUE, 1000)
             .build()
 
         val uploadWorkRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java)
@@ -42,13 +44,37 @@ class MainActivity : AppCompatActivity() {
 
         workManager.enqueue(uploadWorkRequest)
 
+        //incase of work chaining
+        //workManager.beginWith(name of the worker).then(name of the worker).then(name of the worker).enqueue()
+
+        //parallel workers
+
+
         workManager.getWorkInfoByIdLiveData(uploadWorkRequest.id).observe(this, Observer {
             text.text = it.state.name
             Log.e(
                 "MainAct",
                 " Current state name " + text
             )
+            if (it.state.isFinished) {
+                val data = it.outputData
+                val msg: String? = data.getString(UploadWorker.KEY_WORKER)
+                Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+
+            }
         }
         )
+    }
+
+    private fun setPeriodicWorkRequest(){
+
+        val workManager1 = WorkManager.getInstance(applicationContext)
+        val periodicWorkRequest = PeriodicWorkRequest
+            .Builder(UploadWorker::class.java,16,TimeUnit.MINUTES)
+            .build()
+
+        workManager1.enqueue(periodicWorkRequest)
+
+
     }
 }
--- app/src/main/java/com/amitsalunke/workmanager/UploadWorker.kt
+++ app/src/main/java/com/amitsalunke/workmanager/UploadWorker.kt
@@ -6,15 +6,28 @@ import androidx.work.Data
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.util.*
 
 class UploadWorker(context: Context, params: WorkerParameters) : Worker(context, params) {
+    companion object {
+        val KEY_WORKER: String = "Key_Worker"
+    }
+
     override fun doWork(): Result {
         try {
             val count: Int = inputData.getInt(MainActivity.KEY_COUNT_VALUE, 0)
             for (i in 0..count) {
                 Log.e("UploadWorker", " current status of the task $i")
             }
-            return Result.success()
+            val time = SimpleDateFormat("dd/M/yyyy h:mm:ss")
+            val currentDate = time.format(Date())
+
+            val outPutData = Data.Builder()
+                .putString(KEY_WORKER, currentDate)
+                .build()
+
+            return Result.success(outPutData)
         } catch (ex: Exception) {
             Log.e("UploadWorker", "Something went wrong : $ex")
             return Result.failure()
